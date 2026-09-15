89f3b1d96fb72a135c4065afc5508992d30fc6fd
userofbricks <userofbricks11@gmail.com>
2026-09-15 10:24:10 -0700

    latest animation changes

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -11,11 +11,25 @@ public class PlayerControler : MonoBehaviour
 
     Rigidbody2D rb;
     private float moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Is_walking", Mathf.Abs(moveInput));
+        if (moveInput > 0.1f)
+        {
+            transform.localScale = new Vector2(1, 1);
+        } else if (moveInput < -0.1f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
     }
 
     private void FixedUpdate() {
